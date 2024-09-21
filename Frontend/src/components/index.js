af2e129c079df1c82480a59db30a3e4d498df6bf
Mohamed Hamed <mohamed.hamed@outlook.co.nz>
2024-09-21 21:26:19 +1200

    Refactoring app component

---
+++ Frontend/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as TodoItems } from './TodoItems';
+export { default as AddTodoItem } from './AddTodoItem';
